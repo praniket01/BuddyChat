a20282acca901fb99178fcea3e963fb50e71c51a
praniket01 <praniketbodke01@gmail.com>
2025-05-18 17:15:46 +0530

    changes in tailwind.config.js  files

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,5 +1,8 @@
-// import daisyui from "daisyui"
-
-// export default{
-//     plugins:[daisyui]
-// }
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
