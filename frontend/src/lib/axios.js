2d4b4a98454a84e418a65f2a9862d7af328d4c8f
praniket01 <praniketbodke01@gmail.com>
2025-05-19 20:57:18 +0530

    changed base url

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,11 +1,11 @@
 import axios from "axios"
 
 
-const BASE_URL = import.meta.env.MODE ==='development' ? 'http://localhost:5001/api' : '/api'
+const BASE_URL = import.meta.env.MODE ==='development' ? 'http://localhost:5001/api' : 'https://buddychat-2ptu.onrender.com/api'
 
 
 
 export const axioInstance = axios.create({
-    baseURL:"http://localhost:5001/api",
+    baseURL: BASE_URL,
     withCredentials: true
 })
