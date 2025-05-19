6414148737c30e48b66226a2979604fdc0868dee
praniket01 <praniketbodke01@gmail.com>
2025-05-19 21:05:00 +0530

    devDependencies changes

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 
-const BASE_URL = import.meta.env.MODE ==='development' ? 'http://localhost:5001/api' : 'https://buddychat-2ptu.onrender.com/api'
+const BASE_URL = import.meta.env.MODE ==='development' ? 'http://localhost:5001/api' : '/api'
 
 
 
